c6daf5fde90e6244623d5c62ab18615b2ec504a1
Franz Geiger <franz@fairphone.com>
2017-06-30 17:00:22 +0200

    Remove redundant BOARD_SEPOLICY_UNION
    BOARD_SEPOLICY_UNION is no longer required - all files found in
    BOARD_SEPOLICY_DIRS are implicitly unioned.
    
    FPIIM-1306
    
    Change-Id: Ide813be902ee2b08a5f1b0a413816002a44dd9d9

--- sepolicy/Android.mk
+++ sepolicy/Android.mk
@@ -1,7 +1,3 @@
 # Fairphone specific SELinux policy variable definitions
 BOARD_SEPOLICY_DIRS += \
        device/fairphone_devices/sepolicy 
-
-BOARD_SEPOLICY_UNION += \
-       file_contexts \
-       su.te
