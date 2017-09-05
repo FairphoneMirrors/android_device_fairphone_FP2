3d103e87a7c3057a61fa67a1eb3d85942d38d1bf
Borjan Tchakaloff <borjan@fairphone.com>
2017-09-05 17:10:26 +0200

    Only wipe cached resolution sizes for swapped camera
    Invalidate the cached resolution sizes only for the swapped cameras.
    The previous behaviour was to wipe the cached resolution sizes for both
    cameras whenever one of them was changed. Considering that this cache
    exists to limit the resources spent on accessing the camera capabilities,
    do not wipe it if it is not needed.
    
    The "any camera changed" property is now useless, no need to read it.
    
    FPIIM-1291
    
    Change-Id: I0ac53d86b76469f734467fb0a9bb36a3c99f29bf

--- camera_detect/init.fp.camera_preference_wipe.sh
+++ camera_detect/init.fp.camera_preference_wipe.sh
@@ -7,15 +7,14 @@ readonly FILES_TO_WIPE="\
     com.google.android.GoogleCamera/shared_prefs/com.google.android.GoogleCamera_preferences.xml \
     com.android.camera2/shared_prefs/com.android.camera2_preferences.xml
 "
-readonly PREFERENCE_TO_WIPE_RES_CACHE="CachedSupportedPictureSizes_Build_Camera"
+readonly PREFERENCE_TO_WIPE_CACHE_FRONT="CachedSupportedPictureSizes_Build_Camera1"
 readonly PREFERENCE_TO_WIPE_RES_FRONT="pref_camera_picturesize_front_key"
+readonly PREFERENCE_TO_WIPE_CACHE_MAIN="CachedSupportedPictureSizes_Build_Camera0"
 readonly PREFERENCE_TO_WIPE_RES_MAIN="pref_camera_picturesize_back_key"
 
-readonly ANY_CAM_CHANGED_PROPERTY="fp2.cam.any.changed"
 readonly FRONT_CAM_CHANGED_PROPERTY="fp2.cam.front.changed"
 readonly MAIN_CAM_CHANGED_PROPERTY="fp2.cam.main.changed"
 
-readonly ANY_CAM_CHANGED="$(getprop "${ANY_CAM_CHANGED_PROPERTY}")"
 readonly FRONT_CAM_CHANGED="$(getprop "${FRONT_CAM_CHANGED_PROPERTY}")"
 readonly MAIN_CAM_CHANGED="$(getprop "${MAIN_CAM_CHANGED_PROPERTY}")"
 
@@ -29,18 +28,15 @@ function _remove_preferences() {
     local access_bits="$(stat -c '%a' "${file_to_wipe}")"
     local context="$(ls -Z "${file_to_wipe}" | grep -o "u:object_r:[^ ]*")"
 
-    if [ "${ANY_CAM_CHANGED}" == "1" ]
-        then
-        sed -i "/${PREFERENCE_TO_WIPE_RES_CACHE}/d" "${file_to_wipe}"
-    fi
-
     if [ "${FRONT_CAM_CHANGED}" == "1" ]
         then
+        sed -i "/${PREFERENCE_TO_WIPE_CACHE_FRONT}/d" "${file_to_wipe}"
         sed -i "/${PREFERENCE_TO_WIPE_RES_FRONT}/d" "${file_to_wipe}"
     fi
 
     if [ "${MAIN_CAM_CHANGED}" == "1" ]
         then
+        sed -i "/${PREFERENCE_TO_WIPE_CACHE_MAIN}/d" "${file_to_wipe}"
         sed -i "/${PREFERENCE_TO_WIPE_RES_MAIN}/d" "${file_to_wipe}"
     fi
 
